b56b240c52842749757a2ca5074a26b41b11890e
Cpzty <Cristianpzty@gmail.com>
2019-08-27 23:50:15 -0600

    variables cuantitativas

--- analisis exploratorio.R
+++ analisis exploratorio.R
@@ -1,5 +1,6 @@
 #setwd("C:/Users/Cristian/Documents/uvg2019 2do semestre/data science/proyectos/proyecto2/data/Guatemala")
 library("readxl")
+library("dplyr")
 
 historia <- read_excel("Catálogo Guatemala 2018-2019.xlsx", sheet = 1)
 
@@ -10,3 +11,52 @@ names(historia)[names(historia) == "%..24"] <- "Porcentaje precio"
 names(historia)[names(historia) == "%..26"] <- "Porcentaje comision"
 
 clean_historia <- historia[1:31]
+clean_historia[is.na(clean_historia$`Precio Catalogo`),] <- 0
+checkformin_precio <- clean_historia[clean_historia$`Precio Catalogo` !=0,] 
+#solo par ver precio catalogo lo demas no esta limpio
+summary(checkformin_precio)
+qqnorm(checkformin_precio$`Precio Catalogo`, main = "Precio catalogo", col = 2)
+qqline(checkformin_precio$`Precio Catalogo`, col = 3)
+
+
+#limpieza precios venta sin iva 
+clean_historia[is.na(clean_historia$`Precio Vta s/iva`),] <- 0
+checkformin_venta_sin_iva <- clean_historia[clean_historia$`Precio Vta s/iva` !=0,] 
+summary(checkformin_venta_sin_iva)
+qqnorm(checkformin_precio$`Precio Vta s/iva`, main = "Precio venta sin iva", col = 2)
+qqline(checkformin_precio$`Precio Vta s/iva`, col = 3)
+#pronostico
+qqnorm(clean_historia$Pronostico, main = "Pronostico", col = 2)
+qqline(clean_historia$Pronostico, col = 3)
+#unidades vendidas
+summary(clean_historia$`Unidades Vendidas`)
+qqnorm(clean_historia$`Unidades Vendidas`, main = "Unidades vendidas", col = 2)
+qqline(clean_historia$`Unidades Vendidas`, col = 3)
+#venta neta sin iva
+clean_historia[is.na(clean_historia$`Venta Neta s/iva`),] <- 0
+checkformin_ventaneta_sin_iva <- clean_historia[clean_historia$`Venta Neta s/iva` !=0,] 
+summary(checkformin_venta_sin_iva)
+qqnorm(checkformin_precio$`Venta Neta s/iva`, main = "venta sin iva", col = 2)
+qqline(checkformin_precio$`Venta Neta s/iva`, col = 3)
+#costo
+checkformin_costo <- clean_historia$Costo %>% replace(.=="NULL", NA)
+checkformin_costo[is.na(checkformin_costo)] <- 0
+checkformin_costo <- checkformin_costo[checkformin_costo !=0]
+#todavia son strings
+summary(checkformin_costo)
+checkformin_costo <- as.numeric(checkformin_costo)
+summary(checkformin_costo)
+qqnorm(checkformin_costo, main = "Costo", col = 2)
+qqline(checkformin_costo, col = 3)
+#utilidad
+summary(clean_historia$Utilidad)
+qqnorm(clean_historia$Utilidad ,main = "Utilidad", col = 2)
+qqline(clean_historia$Utilidad, col = 3)
+#margen
+summary(clean_historia$Margen)
+qqnorm(clean_historia$Margen ,main = "Margen", col = 2)
+qqline(clean_historia$Margen, col = 3)
+#ratio
+summary(clean_historia$Ratio)
+qqnorm(clean_historia$Ratio ,main = "Ratio", col = 2)
+qqline(clean_historia$Ratio, col = 3)
