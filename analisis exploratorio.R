86f67781c6aa58271c13b2517f4f6658388f309b
Ana lucia Diaz <LEPPEDIAZ@users.noreply.github.com>
2019-09-01 22:27:50 -0600

    cluster 2

--- analisis exploratorio.R
+++ analisis exploratorio.R
@@ -344,3 +344,87 @@ fviz_cluster(km, data = historia[,1:29],geom = "point", ellipse.type = "norm")
 hc.cut<-hcut(historia[,1:29], k=3, hc_method = "complete")
 fviz_dend(hc.cut, show_labels = FALSE, rect = TRUE)
 fviz_cluster(hc.cut, ellipse.type = "convex")
+
+#cluster 2
+
+#k-medias
+datos<-Paginacion20152019
+Paginacion20152019Completo<-Paginacion20152019[complete.cases(Paginacion20152019),]
+km<-kmeans(Paginacion20152019[,1:17],3)
+datos$grupo<-km$cluster
+
+g1<- datos[datos$grupo==1,]
+prop.table(table(g1$Species))*100
+nrow(g1)
+
+
+g2<- datos[datos$grupo==2,]
+prop.table(table(g2$Species))*100
+g3<- datos[datos$grupo==3,]
+prop.table(table(g3$Species))*100
+
+plotcluster(Paginacion20152019[,1:17],km$cluster) #grafica la ubicación de los clusters
+
+#Clustering jerárquico
+hc<-hclust(dist(Paginacion20152019[,1:17])) #Genera el clustering jerárquico de los datos
+plot(hc) #Genera el dendograma
+rect.hclust(hc,k=3) #Dibuja el corte de los grupos en el gráfico
+groups<-cutree(hc,k=3) #corta el dendograma, determinando el grupo de cada fila
+datos$gruposHC<-groups
+
+
+g1HC<-datos[datos$gruposHC==1,]
+g2HC<-datos[datos$gruposHC==2,]
+g3HC<-datos[datos$gruposHC==3,]
+
+#Fuzzy C-Means
+fcm<-cmeans(Paginacion20152019[,1:17],3)
+datos$FCGrupos<-fcm$cluster
+datos<-cbind(datos,fcm$membership)
+
+#Mixture of gaussians
+mc<-Mclust(Paginacion20152019[,1:17],3)
+plot(mc, what = "classification", main="MClust Classification")
+datos$mxGau<-mc$classification
+g1MC<-datos[datos$mxGau==1,]
+g2MC<-datos[datos$mxGau==2,]
+g3MC<-datos[datos$mxGau==3,]
+
+#Método de la silueta para las k-medias
+silkm<-silhouette(km$cluster,dist(Paginacion20152019[,1:17]))
+mean(silkm[,3]) #0.55, no es la mejor partición pero no está mal
+
+#Método de la silueta para clustering jerárquico
+silch<-silhouette(groups,dist(Paginacion20152019[,1:17]))
+mean(silch[,3]) #0.51, no es la mejor partición pero no está mal
+
+#Método de la silueta para fuzzy cmeans
+silfcm<-silhouette(fcm$cluster,dist(Paginacion20152019[,1:17]))
+mean(silfcm[,3]) #0.54, no es la mejor partición pero no está mal
+
+#Método de la silueta para mixture of gaussians
+silmg<-silhouette(mc$classification,dist(Paginacion20152019[,1:17]))
+mean(silmg[,3]) #0.50, no es la mejor partición pero no está mal
+
+#Método de Ward para determinar el número correcto de clusteres con k-medias
+#Para saber cual es el mejor numero de clusters
+wss <- (nrow(Paginacion20152019[,1:17])-1)*sum(apply(Paginacion20152019[,1:17],2,var))
+
+for (i in 2:10) 
+  wss[i] <- sum(kmeans(Paginacion20152019[,1:17], centers=i)$withinss)
+
+plot(1:10, wss, type="b", xlab="Number of Clusters",  ylab="Within groups sum of squares")
+
+#Paquete para saber el mejor n�mero de clusters
+nb <- NbClust(Paginacion20152019[,1:17], distance = "euclidean", min.nc = 2,
+              max.nc = 10, method = "complete", index ="all")
+
+#Visualizaci�n de los clusters con factoextra
+#Visualizaci�n de las k-medias
+fviz_cluster(km, data = Paginacion20152019[,1:17],geom = "point", ellipse.type = "norm")
+
+#Visualizaci�n de cluster jer�rquico
+hc.cut<-hcut(Paginacion20152019[,1:17], k=3, hc_method = "complete")
+fviz_dend(hc.cut, show_labels = FALSE, rect = TRUE)
+fviz_cluster(hc.cut, ellipse.type = "convex")
+
