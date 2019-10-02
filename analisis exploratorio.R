9301af6f9650e70c6d3d575bd4777b9097949289
Ana lucia Diaz <LEPPEDIAZ@users.noreply.github.com>
2019-10-02 02:09:42 -0600

    fixes

--- analisis exploratorio.R
+++ analisis exploratorio.R
@@ -687,41 +687,41 @@ print(co2ts)
 library(ggfortify)
 autoplot(co2ts, ts.colour = "blue", ts.linetype = "dashed")
 autoplot(acf(co2ts, plot = FALSE)
-         autoplot(stl(co2ts, s.window = "periodic"), ts.colour = "blue")nsdiffs(co2ts)
-         ndiffs(co2ts)
-         diff.co2ts<-autoplot(diff(co2ts), ts.linetype = "dashed", ts.colour = "darkmagenta")
-         diff.co2ts
-         autoplot(acf(diff(co2ts), plot = FALSE))
-         diffco2<-diff(co2ts)
-         boxplot(diffco2~cycle(diffco2))
-         diff.co2ts.12<-diff(co2ts, lag = 12)
-         autoplot(diff.co2ts.12, ts.colour = "darkorange4", ts.linetype = "dashed")
-         library(tseries)
-         adf<-adf.test(diff.co2ts.12)
-         adf$p.value
-         kpss<-kpss.test(diff.co2ts.12)
-         kpss$p.value
-         autoplot(acf(diff.co2ts.12, plot = FALSE))
-         library(forecast)
-         arima1<- Arima(co2ts, order=c(0,1,2), seasonal=list(order=c(0,1,1),period=12))
-         arima2<- Arima(co2ts, order=c(1,1,0), seasonal=list(order=c(2,1,0),period=12))
-         arima3<- Arima(co2ts, order=c(1,1,2), seasonal=list(order=c(2,1,1),period=12))
-         arima4<- Arima(co2ts, order=c(1,1,1), seasonal=list(order=c(2,1,1),period=12))
-         arima5<- Arima(co2ts, order=c(1,1,2), seasonal=list(order=c(1,1,1),period=12))
-         arima6<- Arima(co2ts, order=c(0,1,1), seasonal=list(order=c(0,1,1),period=12))
-         arima7<- Arima(co2ts, order=c(1,1,0), seasonal=list(order=c(1,1,0),period=12))
-         AIC(arima1,arima2,arima3,arima4,arima5,arima6,arima7)
-         BIC(arima1,arima2,arima3,arima4,arima5,arima6,arima7)
-         autoplot(pacf(arima6$residuals, plot = FALSE))
-         ggtsdiag(arima6)
-         bp <- Box.test(arima6$residuals) # Test de Box-Pierce
-         bp$p.value
-         lb <- Box.test(arima6$residuals, type="Ljung-Box") # Test de Ljung-Box
-         lb$p.value
-         jb <- jarque.bera.test(arima6$residuals) # Test de Jarque-Bera
-         jb$p.value
-         sht<-shapiro.test(arima6$residuals) $ # Test de Shapiro-Wilk
-           sht$p.value
-         auto.arima(co2ts, stepwise = FALSE, approximation = FALSE)
-         forecast1<-forecast(arima6, level = c(95), h = 50)
-         autoplot(forecast1)
+autoplot(stl(co2ts, s.window = "periodic"), ts.colour = "blue")nsdiffs(co2ts)
+ndiffs(co2ts)
+diff.co2ts<-autoplot(diff(co2ts), ts.linetype = "dashed", ts.colour = "darkmagenta")
+diff.co2ts
+autoplot(acf(diff(co2ts), plot = FALSE))
+diffco2<-diff(co2ts)
+boxplot(diffco2~cycle(diffco2))
+diff.co2ts.12<-diff(co2ts, lag = 12)
+autoplot(diff.co2ts.12, ts.colour = "darkorange4", ts.linetype = "dashed")
+library(tseries)
+adf<-adf.test(diff.co2ts.12)
+adf$p.value
+kpss<-kpss.test(diff.co2ts.12)
+kpss$p.value
+autoplot(acf(diff.co2ts.12, plot = FALSE))
+library(forecast)
+arima1<- Arima(co2ts, order=c(0,1,2), seasonal=list(order=c(0,1,1),period=12))
+arima2<- Arima(co2ts, order=c(1,1,0), seasonal=list(order=c(2,1,0),period=12))
+arima3<- Arima(co2ts, order=c(1,1,2), seasonal=list(order=c(2,1,1),period=12))
+arima4<- Arima(co2ts, order=c(1,1,1), seasonal=list(order=c(2,1,1),period=12))
+arima5<- Arima(co2ts, order=c(1,1,2), seasonal=list(order=c(1,1,1),period=12))
+arima6<- Arima(co2ts, order=c(0,1,1), seasonal=list(order=c(0,1,1),period=12))
+arima7<- Arima(co2ts, order=c(1,1,0), seasonal=list(order=c(1,1,0),period=12))
+AIC(arima1,arima2,arima3,arima4,arima5,arima6,arima7)
+BIC(arima1,arima2,arima3,arima4,arima5,arima6,arima7)
+autoplot(pacf(arima6$residuals, plot = FALSE))
+ggtsdiag(arima6)
+bp <- Box.test(arima6$residuals) # Test de Box-Pierce
+bp$p.value
+lb <- Box.test(arima6$residuals, type="Ljung-Box") # Test de Ljung-Box
+lb$p.value
+jb <- jarque.bera.test(arima6$residuals) # Test de Jarque-Bera
+jb$p.value
+sht<-shapiro.test(arima6$residuals) $ # Test de Shapiro-Wilk
+sht$p.value
+auto.arima(co2ts, stepwise = FALSE, approximation = FALSE)
+forecast1<-forecast(arima6, level = c(95), h = 50)
+autoplot(forecast1)
